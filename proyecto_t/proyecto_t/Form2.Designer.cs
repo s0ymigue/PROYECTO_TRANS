5dd29a0942434b1b9d87f8e461ef626925607138
s0ymigue <pemm080223hdfrnga2@cbtis217.edu.mx>
2024-10-25 13:12:32 -0600

    Formularios

---
+++ proyecto_t/proyecto_t/Form2.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC1_PC33
+ * Date: 25/10/2024
+ * Time: 01:11 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace proyecto_t
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// Form2
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "Form2";
+			this.Name = "Form2";
+		}
+	}
+}
---
+++ proyecto_t/proyecto_t/Form2.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC1_PC33
+ * Date: 25/10/2024
+ * Time: 01:11 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace proyecto_t
+{
+	/// <summary>
+	/// Description of Form2.
+	/// </summary>
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
